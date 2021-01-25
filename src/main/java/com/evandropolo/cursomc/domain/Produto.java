0286df4395359160dfc47ff89c71965b2a3619ac
Evnadro Polo <evandropolo@yahoo.com.br>
2021-01-25 12:03:40 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/evandropolo/cursomc/domain/Produto.java
+++ src/main/java/com/evandropolo/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.evandropolo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -32,15 +36,27 @@ public class Produto  implements Serializable {
 			)
 	private List<Categoria> categoria = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
 	}
-
+	
 	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedido(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x: itens) {
+			lista.add(x.getPedido());
+		}
+		
+		return lista;
 	}
 
 	public Integer getId() {
@@ -74,6 +90,14 @@ public class Produto  implements Serializable {
 	public void setCategoria(List<Categoria> categoria) {
 		this.categoria = categoria;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -99,7 +123,5 @@ public class Produto  implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
